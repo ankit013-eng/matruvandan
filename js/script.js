ed66f83a7cb6cb64647e860517f9167789f938e8
Ankit Katiyar <ankitkatiyar013@gmail.com>
2026-02-08 20:21:03 +0530

    commiting chnages till 8:20

--- js/script.js
+++ js/script.js
@@ -12,4 +12,36 @@ function toggleMenu() {
   document.getElementById("navLinks").classList.toggle("active");
 }
 console.log("Matru Vandan Site Loaded");
+function generateID() {
+  const name = document.getElementById("name").value;
+  const role = document.getElementById("role").value;
+
+  if (!name || !role) {
+    alert("Please fill all details");
+    return;
+  }
+
+  // Create random member ID
+  const memberID = "MV" + Math.floor(1000 + Math.random() * 9000);
+
+  // Display data
+  document.getElementById("memberName").innerText = name;
+  document.getElementById("memberRole").innerText = role;
+  document.getElementById("memberID").innerText = memberID;
+
+  // Show card
+  document.getElementById("idCard").style.display = "block";
+
+  // Clear previous QR
+  document.getElementById("qrcode").innerHTML = "";
+
+  // Generate QR
+  new QRCode(document.getElementById("qrcode"), {
+    text: "Member ID: " + memberID + " | Name: " + name,
+    width: 100,
+    height: 100
+  });
+}
+
+
 
